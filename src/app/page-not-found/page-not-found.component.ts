0dfdcaa7653d0e894068f88d3c95cf918188b25e
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-10-09 22:26:25 -0700

    Add About Page and 404 Page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })
